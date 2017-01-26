b64c37eee1b6138276179a396d00dd64aa567fc8
f-reichel <3razor16@gmail.com>
2017-01-26 23:28:42 +0100

    add settings view

--- routes.js
+++ routes.js
@@ -8,12 +8,14 @@ module.exports = [
   { method: 'GET', path: '/signup', config: Accounts.signup },
   { method: 'GET', path: '/login', config: Accounts.login },
   { method: 'POST', path: '/login', config: Accounts.authenticate },
+  { method: 'POST', path: '/register', config: Accounts.register },
   { method: 'GET', path: '/logout', config: Accounts.logout },
+  { method: 'GET', path: '/settings', config: Accounts.viewSettings },
+  { method: 'POST', path: '/settings', config: Accounts.updateSettings },
 
   { method: 'GET', path: '/home', config: Twitter.home },
   { method: 'GET', path: '/report', config: Twitter.report },
   { method: 'POST', path: '/tweet', config: Twitter.write },
-  { method: 'POST', path: '/register', config: Accounts.register },
 
   {
     method: 'GET',
