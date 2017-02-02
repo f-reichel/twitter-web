7718c879cc20ba0923cf82b79c67672d3c741cdd
f-reichel <3razor16@gmail.com>
2017-02-02 22:48:43 +0100

    enable seeding also for production mode

--- app/models/db.js
+++ app/models/db.js
@@ -14,18 +14,20 @@ mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function () {
   console.log('Mongoose connected to ' + dbURI);
-  if (process.env.NODE_ENV != 'production') {
-    var seeder = require('mongoose-seeder');
-    const data = require('./data.json');
-    const Tweet = require('./tweet');
-    const User = require('./user');
-    seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
-      // console.log('preloading Test Data');
-      // console.log(dbData);
+  
+  // if (process.env.NODE_ENV != 'production') {
+  var seeder = require('mongoose-seeder');
+  const data = require('./data.json');
+  const Tweet = require('./tweet');
+  const User = require('./user');
+  seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
+      console.log('preloading Test Data');
+      console.log(dbData);
     }).catch(err => {
       console.log(error);
     });
-  }
+  
+  // }
 });
 
 mongoose.connection.on('error', function (err) {
